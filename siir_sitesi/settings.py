9574a867c88af09b5e0ac977bfa05c09ff4820d5
sefa yılmaz <136167511+8efay@users.noreply.github.com>
2025-05-25 16:14:00 +0300

    başarılı

--- siir_sitesi/settings.py
+++ siir_sitesi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c$l6bxy0td5jfk2z2435n%5h#b8@6tsl%rj4e(w#c%e32%1qk$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['siiragac.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['siiragaci.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
